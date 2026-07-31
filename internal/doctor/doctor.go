b8b7bee1eef4112ca9f5d3cc3f81fb4cc85a1403
ahmadalguydi <ahmadalgaidy@hotmail.com>
2026-07-31 13:08:14 +0300

    refactor: deduplicate optional variable checks

--- internal/doctor/doctor.go
+++ internal/doctor/doctor.go
@@ -784,16 +784,8 @@ func CheckVariables(cfg *complytime.WorkspaceConfig, healthData []ProviderHealth
 				}
 			}
 			for _, group := range ph.OptionalTargetVariableGroups {
-				members := strings.Split(group, "|")
 				targetTotal++
-				found := false
-				for _, m := range members {
-					if _, ok := target.Variables[m]; ok {
-						found = true
-						break
-					}
-				}
-				if found {
+				if groupSatisfied(group, target.Variables) {
 					targetResolved++
 				} else {
 					missingTargetVars = append(missingTargetVars,
@@ -892,13 +884,9 @@ func CheckVariables(cfg *complytime.WorkspaceConfig, healthData []ProviderHealth
 						})
 					}
 					for _, group := range ph.OptionalTargetVariableGroups {
-						members := strings.Split(group, "|")
 						detailStatus := StatusFail
-						for _, m := range members {
-							if _, ok := target.Variables[m]; ok {
-								detailStatus = StatusPass
-								break
-							}
+						if groupSatisfied(group, target.Variables) {
+							detailStatus = StatusPass
 						}
 						details = append(details, CheckResult{
 							Name:    fmt.Sprintf("variables/%s/detail", ph.EvaluatorID),
@@ -1054,6 +1042,15 @@ func countResolved(required []string, vars map[string]string) (resolved, total i
 		}
 	}
 	return resolved, total
+}
+
+func groupSatisfied(group string, vars map[string]string) bool {
+	for _, member := range strings.Split(group, "|") {
+		if _, ok := vars[member]; ok {
+			return true
+		}
+	}
+	return false
 }
 
 func unmappedReason(resolver PolicyGraphResolver, resolveFailures int) string {
